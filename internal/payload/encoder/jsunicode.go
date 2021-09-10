6c76434a2d7a7508d967adb2e6e1b7a2996ca688
Simon Kirillov <svkirillov3@gmail.com>
2021-09-10 13:17:19 +0700

    Fix JSUnicode encoder (#73)
    * Fix JSUnicode encoder
    
    * Fix nginx config

--- internal/payload/encoder/jsunicode.go
+++ internal/payload/encoder/jsunicode.go
@@ -1,5 +1,12 @@
 package encoder
 
+import (
+	"fmt"
+
+	"golang.org/x/text/encoding/unicode"
+	"golang.org/x/text/transform"
+)
+
 type JSUnicodeEncoder struct {
 	name string
 }
@@ -11,16 +18,16 @@ func (enc JSUnicodeEncoder) GetName() *string {
 }
 
 func (enc JSUnicodeEncoder) Encode(data string) (string, error) {
-	ret := ""
-	// TODO: check how it works with unicode multibytes
-	for _, c := range data {
-		if c < 'a' || c > 'z' &&
-			c < 'A' || c > 'Z' &&
-			c < '0' || c > '9' {
-			ret += string(c)
-			continue
-		}
-		ret += "\\u00" + string(c)
+	encoder := unicode.UTF16(unicode.BigEndian, unicode.IgnoreBOM).NewEncoder()
+	utf16beStr, _, err := transform.Bytes(encoder, []byte(data))
+	if err != nil {
+		return "", err
 	}
+
+	ret := ""
+	for i := 0; i < len(utf16beStr); i += 2 {
+		ret += fmt.Sprintf("\\u%02x%02x", utf16beStr[i], utf16beStr[i+1])
+	}
+
 	return ret, nil
 }
