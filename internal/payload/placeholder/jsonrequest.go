34cd393d80742d3a3087e4e97b434a384b556491
Simon Kirillov <svkirillov3@gmail.com>
2021-10-07 15:23:50 +0700

    Rename JSONBody to JSONRequest

---
+++ internal/payload/placeholder/jsonrequest.go
@@ -0,0 +1,33 @@
+package placeholder
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+	"strings"
+
+	"github.com/wallarm/gotestwaf/internal/payload/encoder"
+)
+
+func JSONRequest(requestURL, payload string) (*http.Request, error) {
+	reqURL, err := url.Parse(requestURL)
+	if err != nil {
+		return nil, err
+	}
+
+	param, err := RandomHex(Seed)
+	if err != nil {
+		return nil, err
+	}
+	encodedPayload, err := encoder.Apply("JSUnicode", payload)
+	if err != nil {
+		return nil, err
+	}
+	jsonPayload := fmt.Sprintf("{\"test\":true, \"%s\": \"%s\"}", param, encodedPayload)
+	req, err := http.NewRequest("POST", reqURL.String(), strings.NewReader(jsonPayload))
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("Content-Type", "application/json")
+	return req, nil
+}
--- internal/payload/placeholder/placeholder.go
+++ internal/payload/placeholder/placeholder.go
@@ -21,8 +21,8 @@ func (p Placeholder) SOAPBody(url, data string) (*http.Request, error) {
 	return SOAPBody(url, data)
 }
 
-func (p Placeholder) JSONBody(url, data string) (*http.Request, error) {
-	return JSONBody(url, data)
+func (p Placeholder) JSONRequest(url, data string) (*http.Request, error) {
+	return JSONRequest(url, data)
 }
 
 func (p Placeholder) URLParam(url, data string) (*http.Request, error) {
