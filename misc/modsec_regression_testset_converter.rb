19b7a3237c3018fe3d8e543886944debdf8e59d4
Simon Kirillov <skirillov@fastmail.com>
2025-07-11 19:40:40 +0200

    Fix `modsec_regression_testset_converter.rb` (#273)
    * Fix modsec_regression_testset_converter.rb
    
    * Fix .gitignore
    
    * Fix modsec_regression_testset_converter.rb

--- misc/modsec_regression_testset_converter.rb
+++ misc/modsec_regression_testset_converter.rb
@@ -22,7 +22,7 @@ def separate_negative_and_positive_tests(tests)
   result = { 'positive' => [], 'false-positive' => [] }
 
   tests.each do |t|
-    if t['stages'][0]['stage']['output'].keys.include? 'no_log_contains' and t['stages'][0]['stage']['output']['no_log_contains'].match(/^id/i)
+    if t['stages'][0]['output']['log'].keys.include? 'no_expect_ids' and t['stages'][0]['output']['log']['no_expect_ids'].length() == 1
       result['false-positive'] << t
     else
       result['positive'] << t
@@ -43,7 +43,7 @@ def get_rules_paranoia_level(test_set)
     paranoia = line.match(/DETECTION_PARANOIA_LEVEL "@.*(\d+)"/i)[1] if line.match(/DETECTION_PARANOIA_LEVEL "@.*\d+"/i)
     next if paranoia.nil? || id.nil?
 
-    result[id] = paranoia.to_i
+    result[id.to_i] = paranoia.to_i
   end
 
   result
@@ -51,16 +51,16 @@ end
 
 def split_tests_by_req_method(tests)
   result = []
-  test_title = tests['test_title']
+  test_id = tests['test_id']
 
   tests['stages'].each do |stage|
-    case stage['stage']['input']['method']
+    case stage['input']['method']
     when nil
-      result << parse_get_requests(stage['stage']['input'], test_title)
+      result << parse_get_requests(stage['input'], test_id)
     when 'POST'
-      result << parse_post_requests(stage['stage']['input'], test_title)
+      result << parse_post_requests(stage['input'], test_id)
     else
-      result << parse_get_requests(stage['stage']['input'], test_title)
+      result << parse_get_requests(stage['input'], test_id)
     end
   end
 
@@ -152,9 +152,9 @@ crs_testcases.each do |gtw_testcase_name, test_set|
     next unless file.match(/.yaml$/)
 
     parsed_yaml_data = YAML.safe_load(File.read(file))
-    next if parsed_yaml_data['meta']['enabled'] != true
 
-    rule_id = parsed_yaml_data['meta']['name'].split('.')[0]
+    rule_id = parsed_yaml_data['rule_id']
+
     next if paranoia_level[rule_id].nil? || paranoia_level[rule_id] >= max_paranoia_level
     parsed_yaml_data['tests'].each { |t| current_test_set_data << t }
   end
